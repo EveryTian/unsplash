efafcbec2ab6e53bd315973dd1c149c59e409207
EveryTian <haotian_ren@outlook.com>
2018-06-03 10:36:23 +0800

    Part 3

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,7 +1,6 @@
 import { TabNavigator, StackNavigator } from 'react-navigation'
 import React from 'react';
-import { Images, Colors, Metrics } from '../Themes'
-import { StyleSheet, Image } from 'react-native';
+import { Colors } from '../Themes'
 
 import HomeScreen from '../Screens/HomeScreen'
 import BookmarkScreen from '../Screens/BookmarkScreen'
@@ -12,9 +11,23 @@ import UserProfileScreen from '../Screens/UserProfileScreen'
 /* Part 3 */
 /* Part 3.1: Create a StackNavigator that contains the HomeScreen, and the UserProfileScreen */
 /* initialRouteName should be your HomeScreen. Set headerMode to 'float'  */
+const stackNavWithHomeScreen = StackNavigator({
+  HomeScreen: { screen: HomeScreen },
+  UserProfileScreen: { screen: UserProfileScreen },
+}, {
+  initialRouteName: 'HomeScreen',
+  headerMode: 'float',
+});
 
 /* Part 3.2: Create a StackNavigator that contains the BookmarkScreen, and the BookmarkViewerScreen */
 /* initialRouteName should be your BookmarkScreen. Set headerMode to 'float'  */
+const stackNavWithBookmarkScreen = StackNavigator({
+  BookmarkScreen: { screen: BookmarkScreen },
+  BookmarkViewerScreen: { screen: BookmarkViewerScreen },
+}, {
+  initialRouteName: 'BookmarkScreen',
+  headerMode: 'float',
+});
 
 /* Part 3.3: nest both StackNavigators in the TabNavigator below */
 /* Your FeedScreen should point to the StackNavigator that you created in Part 3.1 */
@@ -22,8 +35,8 @@ import UserProfileScreen from '../Screens/UserProfileScreen'
 
 // Manifest of possible screens
 const TabNav = TabNavigator({
-  FeedScreen: { screen: HomeScreen },
-  BookmarkScreen:   { screen: BookmarkScreen },
+  FeedScreen: { screen: stackNavWithHomeScreen },
+  BookmarkScreen: { screen: stackNavWithBookmarkScreen },
 }, {
   // Default config for all screens
   initialRouteName: 'FeedScreen',
@@ -31,6 +44,6 @@ const TabNav = TabNavigator({
     activeTintColor: Colors.black,
     showLabel: false,
   },
-})
+});
 
 export default TabNav
